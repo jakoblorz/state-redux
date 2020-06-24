aa1e4d13fa96b9e5936af9c724058c4569ae318d
Jakob Lorz <jakob@lorz.org>
2020-06-24 14:37:16 +0200

    Initialize Project Structure

---
+++ dist/cjs/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useName = void 0;
+exports.useName = () => {
+    return "name";
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const useName = () => {
+    return "name";
+}
